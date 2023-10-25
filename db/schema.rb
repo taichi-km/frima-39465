7c0015e1e221f73ca63dc444d4a015e156dcb892
taichi-km <torinos947@gmail.com>
2023-10-25 11:38:33 +0900

    basic認証修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_010041) do
-  create_table "items", charset: "utf8", force: :cascade do |t|
-    t.string "string", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
