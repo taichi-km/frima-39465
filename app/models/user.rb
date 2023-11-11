4ec9568f11994ac1ec69f86b2b465ae2a8728f2e
taichi-km <torinos947@gmail.com>
2023-11-11 16:03:20 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :name_last, presence: true
+  validates :name_first, presence: true
+  validates :katakana_last, presence: true
+  validates :katakana_first, presence: true
+  validates :birthday, presence: true
+end
