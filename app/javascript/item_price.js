a69b2b3062187b5b87c2c9acc7ee8e4d5af53f66
taichi-km <torinos947@gmail.com>
2023-11-13 14:48:43 +0900

    商品出品機能の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,15 @@
+function fee () {
+  
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const addProfitDom = document.getElementById("profit");
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    addProfitDom.innerHTML = Math.floor(inputValue - Math.floor(inputValue * 0.1));
+  });
+
+};
+
+window.addEventListener('turbo:load', fee);
