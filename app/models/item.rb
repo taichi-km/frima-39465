90512c0e0aa472628fdb68f208db097b9190073e
taichi-km <torinos947@gmail.com>
2023-11-13 15:36:07 +0900

    レビュー後の修正②

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id,          numericality: { other_than: 1, message: "can't be blank" }
   validates :scheduled_delivery_id,  numericality: { other_than: 1, message: "can't be blank" }
-  validates :price,                  presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price,                  presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
