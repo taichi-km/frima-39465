360f0288f5ee2786686ec80192eb07bd6ffcacc3
taichi-km <torinos947@gmail.com>
2023-11-18 16:10:23 +0900

    userモデルとitemモデルの可読性向上

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,15 +2,21 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   
-  validates :image,                  presence: true
-  validates :name,                   presence: true
-  validates :info,                   presence: true
-  validates :category_id,            numericality: { other_than: 1, message: "can't be blank" }
-  validates :status_id,              numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id,          numericality: { other_than: 1, message: "can't be blank" }
-  validates :scheduled_delivery_id,  numericality: { other_than: 1, message: "can't be blank" }
-  validates :price,                  presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
