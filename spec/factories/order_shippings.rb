35287086379d155b13c1e44038f6e9d5e4364a97
taichi-km <torinos947@gmail.com>
2023-12-03 21:48:50 +0900

    商品購入機能の実装

---
+++ spec/factories/order_shippings.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :order_shipping do
+    
+    token         { "tok_sk39894984938320" }
+    postal_code   { "123-4567" }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    city          { Faker::Address.city }
+    address       { Faker::Address.street_address }
+    building      { "建物" }
+    phone_number  { "0901234567" }
+    association :user
+    association :item
+
+  end
+end
