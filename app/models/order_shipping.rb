6fd2426076b41ed62d9713302627b14abab16720
taichi-km <torinos947@gmail.com>
2023-12-08 14:21:04 +0900

    レビュー後の修正②

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -6,15 +6,13 @@ class OrderShipping
   with_options presence: true do
     validates :token
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "input correctly" }
-  end
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  with_options presence: true do
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "input correctly" }
     validates :user_id
     validates :item_id
   end
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
